390e32a4355f08e4bc31589337d9ddebeff88ac4
Chris Hoeppler <christian.hoeppler@bosch.com>
2020-01-17 13:40:23 +0100

    Install python dependencies with apt-get
    Just use the Ubuntu package management to install sphinx and other
    python dependencies needed to build the docs. There are various
    mechanisms to install python packages (apt, conda, pip, ...) and we only
    provide one default for installation. If the required dependencies are
    already there, don't do anything. This test should be independent of any
    particular package manger, so we just test whether python3 can import
    the respecitve modules.
    
    Signed-off-by: Chris Hoeppler <christian.hoeppler@bosch.com>

--- setup.sh
+++ setup.sh
@@ -1,71 +1,42 @@
 #!/bin/bash
 
-grep_ver="grep -o [0-9.]*"
-grep_perm_denied="grep -o Permission"
-grep_success="grep -o Successfully"
 
-check_install_pkg() {
-    pip_tool="pip"
-    pkg="$1"
-    #Check python-$pkg version, if not available install
-    pkg_ver=$($pip_tool show $pkg 2>&1| grep --line-buffered "^Version" | $grep_ver) 
-    is_pkg_dep_satisfied=$($pip_tool check $pkg 2>&1 | grep "No broken requirements found")
-    if [ -n "$pkg_ver" ] && [ -n "$is_pkg_dep_satisfied" ]; then
-        echo "$pkg version - $pkg_ver found"
-    else
-        echo "$pkg not found or missing dependencies. Installing $pkg..."
-        install_output=$($pip_tool install $pkg 2>&1)
-        if [ -n "$(echo $install_output | $grep_perm_denied)" ]; then 
-            install_output=$($pip_tool install --user $pkg 2>&1)
+check_install_module() {
+    module="$1"
+    pkg="$2"
 
-            if [ -n "$(echo $install_output | $grep_success)" ]; then 
-            pkg_ver=$($pip_tool show $pkg | grep --line-buffered "^Version"  | $grep_ver) 
-            echo "Successfully installed $pkg version $pkg_ver"
-            fi
-        elif [ -n "$(echo $install_output | $grep_success)" ]; then
-            pkg_ver=$($pip_tool show $pkg | grep --line-buffered "^Version"  | $grep_ver) 
-            echo "Successfully installed $pkg version $pkg_ver"
-        else 
-            echo "Unknown error installing $pkg"
-            echo $install_output
-        fi
+    python3 -c "import $module" 2>&1
+    if [ $? -eq 0 ]; then
+        echo "$module already installed."
+        return
     fi
-}
-
-check_install_pip3(){
-
-	pip_ver=$(pip3 --version | grep -o "pip[ 0-9\.]*" | $grep_ver)
-	if [ -n "$pip_ver" ]; then
-		echo "pip $pip_ver found"
-
-	else
-		echo "pip not found, Installing... "
-		install_output=$(sudo apt-get install python3-pip)
-
-        pip_ver=$(pip3 --version | grep -o "pip[ 0-9\.]*" | grep -o "[0-9.]*")
-	    if [ -n "$pip_ver" ]; then
-            echo "Successfully installed pip $pip_ver"
-        else
-            echo "Unknown error installing pip"
-        fi
+    echo "Installing $pkg"
+    sudo apt-get install $pkg
+    if [ $? -ne 0 ]; then
+        echo "Installing $pkg failed.\nAbort."
+        exit 1
     fi
 }
 
 check_install_python3(){
-    python3_ver=$(python3 --version | $grep_ver)
+    if [ $(which python3) ]; then
+        python3_ver=$(python3 --version)
+        echo "$python3_ver already installed."
+        return
+    fi
 
-    if [ -n "$python3_ver" ]; then
-        echo "python3 version - $python3_ver found"
-    else 
-        echo "python3 not found. Installing python3..."
-        sudo apt-get install python3
+    echo "python3 not found. Installing python3..."
+    sudo apt-get install python3
+    if [ $? -ne 0 ]; then
+        echo "Installing python3 failed."
+        exit 1
     fi
 }
 
 check_install_python3
-check_install_pip3
-check_install_pkg "sphinx"
-check_install_pkg "sphinx-rtd-theme"
+check_install_module "sphinx" "python3-sphinx"
+check_install_module "sphinx_rtd_theme" "python3-sphinx-rtd-theme"
 
 #install other project dependencies
 ./deps.sh
+
