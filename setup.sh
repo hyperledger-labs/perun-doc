ddfd772a537fa75e9be67ade2cc22e82c0cde438
Anagha Sukumaran <external.Sukumaran.AnaghaK@in.bosch.com>
2020-01-24 10:21:40 +0100

    Revert to using pip for installing dependencies
    Using pip install with a requirements file will check for appropriate
    versions of dependencies (dependencies installed by other package
    managers like apt-get will also be recognized), and install only the
    missing ones.
    
    Also, apt-get may install different versions of packages depending upon
    the OS (debian, ubuntu etc.,) and their versions. But using pip will
    provide consistent results accross all OS and their versions.
    
    Signed-off-by: Anagha Sukumaran <external.Sukumaran.AnaghaK@in.bosch.com>

--- setup.sh
+++ setup.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-
-check_install_module() {
-    module="$1"
-    pkg="$2"
-
-    python3 -c "import $module" 2>&1
-    if [ $? -eq 0 ]; then
-        echo "$module already installed."
-        return
-    fi
-    echo "Installing $pkg"
-    sudo apt-get install $pkg
-    if [ $? -ne 0 ]; then
-        echo "Installing $pkg failed.\nAbort."
-        exit 1
-    fi
-}
-
 check_install_python3(){
     if [ $(which python3) ]; then
         python3_ver=$(python3 --version)
@@ -34,8 +16,9 @@ check_install_python3(){
 }
 
 check_install_python3
-check_install_module "sphinx" "python3-sphinx"
-check_install_module "sphinx_rtd_theme" "python3-sphinx-rtd-theme"
+
+# On debian based systems, --user flag is used by default when not running in virtualenv or as root
+pip install -r requirements.txt
 
 #install other project dependencies
 ./deps.sh
