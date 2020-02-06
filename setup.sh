13dc69e4d6d86969df72f04b7c0be38c6f5c794f
Anagha K Sukumaran <sukumaran.anaghak@in.bosch.com>
2020-02-06 12:46:05 +0530

    In setup.sh, install pip if not present (#15)
    Also,
    - Remove support for Python 2, as it reached the end of life.
    - Use requirements.txt file to instal python packages.
    - After installing sphinx, check if sphinx-build is callable and print a help message if it is not. 
    - Add the "-p" flag to mkdir command in deps.sh, to suppress errors if the directory already present.
    
    Pip install with requirements file is used for python packages, because
    it will provide consistent results across all OS and their versions.
    Also, the dependencies installed by other package managers like apt-get
    will also be recognized and only the required ones will be installed.
    
    Resolves #13
    
    Signed-off-by: Anagha K Sukumaran <sukumaran.anaghak@in.bosch.com>

--- setup.sh
+++ setup.sh
@@ -1,59 +1,53 @@
 #!/bin/bash
 
-grep_ver="grep -o [0-9.]*"
-grep_perm_denied="grep -o Permission"
-grep_success="grep -o Successfully"
-
-check_install_pkg() {
-    pip_tool="pip$1"
+check_install_module() {
+    module="$1"
     pkg="$2"
-    #Check python-$pkg version, if not available install
-    pkg_ver=$($pip_tool show $pkg 2>&1| grep --line-buffered "^Version" | $grep_ver) 
-    if [ -n "$pkg_ver" ]; then
-        echo "$pkg version - $pkg_ver found"
-    else
-        echo "$pkg not found. Installing $pkg..."
-        install_output=$($pip_tool install $pkg 2>&1)
-        if [ -n "$(echo $install_output | $grep_perm_denied)" ]; then 
-            install_output=$($pip_tool install --user $pkg 2>&1)
-
-            if [ -n "$(echo $install_output | $grep_success)" ]; then 
-            pkg_ver=$($pip_tool show $pkg | grep --line-buffered "^Version"  | $grep_ver) 
-            echo "Successfully installed $pkg version $pkg_ver"
-            fi
-        elif [ -n "$(echo $install_output | $grep_success)" ]; then
-            pkg_ver=$($pip_tool show $pkg | grep --line-buffered "^Version"  | $grep_ver) 
-            echo "Successfully installed $pkg version $pkg_ver"
-        else 
-            echo "Unknown error installing $pkg"
-            echo $install_output
-        fi
-
+    python3 -c "import $module" 2>&1
+    if [ $? -eq 0 ]; then
+        echo "$module already installed."
+        return
+    fi
+    echo "Installing $pkg"
+    sudo apt-get install $pkg
+    if [ $? -ne 0 ]; then
+        echo "Installing $pkg failed.\nAbort."
+        exit 1
     fi
 }
-
-#Check python2 version, if not available install
-python2_ver=$(python --version 2>&1 | $grep_ver)
-python3_ver=$(python3 --version | $grep_ver)
-if [ -n "$python2_ver" ]; then
-    echo "python2 version - $python2_ver found"
-    check_install_pkg "2" "sphinx"
-    check_install_pkg "2" "sphinx-rtd-theme"
-else
-    #Check python3 version, if not available, install python2 and corresponding sphinx
-    if [ -n "$python3_ver" ]; then
-        echo "python3 version - $python3_ver found"
-        #if python3 is installed, check_install_sphinx3
-        check_install_pkg "3" "sphinx"
-        check_install_pkg "3" "sphinx-rtd-theme"
-    
-    else 
-        echo "python2 not found. Installing python2..."
-        sudo apt-get install python2
-        check_install_pkg "2" "sphinx"
-        check_install_pkg "2" "sphinx-rtd-theme"
+check_install_python3(){
+    if [ $(which python3) ]; then
+        python3_ver=$(python3 --version)
+        echo "$python3_ver already installed."
+        return
     fi
-fi
+    echo "python3 not found. Installing python3..."
+    sudo apt-get install python3
+    if [ $? -ne 0 ]; then
+        echo "Installing python3 failed."
+        exit 1
+    fi
+}
+check_install_sphinx(){
+    if ! sphinx-build --v 2>/dev/null; then
+        if [ -x ~/.local/bin/sphinx-build ]; then
+            echo -e "Sphinx tools are installed in ~/.local/bin.\nThis directory is not found in system path, add it to the "'$PATH'" variable"
+            exit 1
+        else
+            echo "'sphinx-build' not callable. Either installation failed or there is a problem with your "'$PATH'"."
+            exit 1
+        fi
+    fi
+}
+check_install_python3
+
+check_install_module "pip" "python3-pip" 
+
+#On debian based systems, --user flag is used by default when not running in virtualenv or as root
+python3 -m pip install -r requirements.txt
+
+check_install_sphinx
 
 #install other project dependencies
 ./deps.sh
+
