cae54450dfff90d1994e5ec40f88082a703306c3
Manoranjith <ponraj.manoranjitha@in.bosch.com>
2020-02-07 16:31:35 +0530

    Add option to run setup in quiet mode (for ci)
    - Also address review comments.
    
    Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

--- setup.sh
+++ setup.sh
@@ -1,36 +1,73 @@
 #!/bin/bash
 
-if [ $(whoami) == root ]; then
-    pkg_manager_cmd="apt-get install -qy" # q provides less verbose o/p. y assumes yes for all prompts.
-else
-    pkg_manager_cmd="sudo apt-get install -qy" # q provides less verbose o/p. y assumes yes for all prompts.
+quiet_mode=false
+
+# Parse command line options
+while [ ! $# -eq 0 ]
+do
+    case "$1" in
+        --help | -h)
+            echo "use --quiet-mode or -q flag to run the script in non-interactive, less verbose mode"
+            exit 0
+            ;;
+        --quiet-mode | -q)
+            quiet_mode=true
+            echo -e "Running setup in quiet mode : non-interactive and less verbose output\n"
+            ;;
+        *)
+            echo "Unknown option provided: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+pkg_manager_cmd="apt-get install"
+
+# if not invoked as root, prefix sudo to package manager command
+if [ ! $(whoami) == root ]; then
+    pkg_manager_cmd="sudo $pkg_manager_cmd"
+fi
+
+# add -qy flags in quiet mode, to provide less verbose output and assume yes for all prompts
+if [ $quiet_mode == true ]; then
+    pkg_manager_cmd="$pkg_manager_cmd -qy"
 fi
 
 check_install_pkg() {
 
-    pkg_name="$1"
+    cmd="$1"
     pkg_version_check_cmd="$2"
-    pkg_install_cmd="$3"
+    pkg_to_install="$3"
 
     if $pkg_version_check_cmd 2>/dev/null; then
-        echo -e "$($pkg_version_check_cmd)pi already installed.\n"
+        echo -e "$($pkg_version_check_cmd) already installed.\n"
         return
     fi
 
-    echo "$pkg_name not found. Installing $pkg_install_cmd."
-    $pkg_manager_cmd $pkg_install_cmd
+    echo "$cmd not found. Installing $pkg_to_install."
+    $pkg_manager_cmd $pkg_to_install
 
     if [ $? -ne 0 ]; then
-        echo "Installing $pkg_install_cmd failed.\Abort."
+        echo "Installing $pkg_to_install failed.\Abort."
         exit 1
     fi
 }
 
 check_install_python_pkg(){
 
+    pip_cmd="python3 -m pip install -r requirements.txt"
+
+    #-q to suppress verbose output and print only error
+    if [ $quiet_mode == true ]; then
+        pip_cmd="$pip_cmd -q"
+    fi
+
     echo -n "Installing python dependencies :........"
-    if python3 -m pip install -q -r requirements.txt; then #-q to suppress verbose output and print only error
+    if $pip_cmd; then
         echo -n -e "Successful\n\n"
+    else
+        exit 1
     fi
 }
 
@@ -64,7 +101,7 @@ check_install_plantuml() {
     echo "java -Djava.awt.headless=true -jar $plantumlPath/plantuml-1.2019.10.jar -failfast2 \"\$@\"" >> plantuml
     chmod +x plantuml
 
-    echo "Plantuml available at:$plantumlPath\n"
+    echo "Plantuml available at: \"$plantumlPath\"\n"
 }
 
 # Install sphinx and required dependencies
